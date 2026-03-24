48fc4426e2603b09ab24600796984d8b54631d1b
NathanRouleau <nathan7.rouleau@gmail.com>
2026-03-24 10:15:08 +0100

    feat: add dbInstance

---
+++ assurmoi/models/index.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+// const db_username = process.env.DB_USERNAME || 'root';
+
+const dbInstance = new Sequelize(`mariadb:// ${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    dialect: 'mariadb',
+    dialectOptions: {
+        ssl: {
+            require: true,
+        }
+    }
+});
+
+module.exports = { 
+    dbInstance 
+};
---
+++ assurmoi/models/user.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const { dbInstance } = require('.');
+
+const User = (Sequelize, DataTypes) => {
+    class User extends Model {}
+    
+    User.init(
+        {
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            firstname: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            lastname: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            email: DataTypes.STRING
+        },
+        {
+            sequelize: dbInstance,
+            modelName: 'User'
+        }
+    )
+    
+    return User;
+}
+
+module.exports = User
